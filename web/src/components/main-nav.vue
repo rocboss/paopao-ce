42740a394585ef1d5442b3433ef7593298b9ca3b
zhanlaoban <zhanlaoban@gmail.com>
2022-08-22 21:35:58 +0800

    add import of LightModeOutlined component

--- web/src/components/main-nav.vue
+++ web/src/components/main-nav.vue
@@ -40,7 +40,7 @@ import { onMounted } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 import { useOsTheme } from 'naive-ui';
-import { DarkModeOutlined, ChevronLeftRound } from '@vicons/material';
+import { LightModeOutlined, DarkModeOutlined, ChevronLeftRound } from '@vicons/material';
 
 const store = useStore();
 const router = useRouter();
