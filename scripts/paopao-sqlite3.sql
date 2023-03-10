7cad1e644323b1ba7d308275eff95858f85db09c
Michael Li <alimy@gility.net>
2023-03-10 17:45:34 +0800

    add postgresql ddl and optimize mysql/sqlite3 ddl

--- scripts/paopao-sqlite3.sql
+++ scripts/paopao-sqlite3.sql
@@ -101,10 +101,10 @@ CREATE TABLE "p_contact" (
   "group_id" integer NOT NULL,
   "remark" text(32) NOT NULL,
   "status" integer NOT NULL,
+  "notice_enable" integer NOT NULL,
   "is_top" integer NOT NULL,
   "is_black" integer NOT NULL,
   "is_del" integer NOT NULL,
-  "notice_enable" integer NOT NULL,
   "created_on" integer NOT NULL,
   "modified_on" integer NOT NULL,
   "deleted_on" integer NOT NULL,
@@ -169,7 +169,8 @@ CREATE TABLE "p_post" (
   "created_on" integer NOT NULL,
   "modified_on" integer NOT NULL,
   "deleted_on" integer NOT NULL,
-  "is_del" integer NOT NULL, `visibility` integer NOT NULL DEFAULT '0',
+  "is_del" integer NOT NULL,
+  "visibility" integer NOT NULL,
   PRIMARY KEY ("id")
 );
 
@@ -313,7 +314,7 @@ CREATE TABLE "p_wallet_statement" (
 -- ----------------------------
 -- Indexes structure for table p_attachment
 -- ----------------------------
-CREATE INDEX "main"."idx_user"
+CREATE INDEX "idx_attachment_user_id"
 ON "p_attachment" (
   "user_id" ASC
 );
@@ -321,15 +322,15 @@ ON "p_attachment" (
 -- ----------------------------
 -- Indexes structure for table p_captcha
 -- ----------------------------
-CREATE INDEX "main"."idx_expired_on"
+CREATE INDEX "idx_captcha_expired_on"
 ON "p_captcha" (
   "expired_on" ASC
 );
-CREATE INDEX "main"."idx_phone"
+CREATE INDEX "idx_captcha_phone"
 ON "p_captcha" (
   "phone" ASC
 );
-CREATE INDEX "main"."idx_use_times"
+CREATE INDEX "idx_captcha_use_times"
 ON "p_captcha" (
   "use_times" ASC
 );
@@ -337,20 +338,52 @@ ON "p_captcha" (
 -- ----------------------------
 -- Indexes structure for table p_comment
 -- ----------------------------
-CREATE INDEX "main"."idx_post"
+CREATE INDEX "idx_comment_post_id"
 ON "p_comment" (
   "post_id" ASC
+);
+CREATE INDEX "idx_comment_user_id"
+ON "p_comment" (
+  "user_id" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_comment_content
+-- ----------------------------
+CREATE INDEX "idx_comment_content_comment_id"
+ON "p_comment_content" (
+  "comment_id" ASC
+);
+CREATE INDEX "idx_comment_content_sort"
+ON "p_comment_content" (
+  "sort" ASC
+);
+CREATE INDEX "idx_comment_content_type"
+ON "p_comment_content" (
+  "type" ASC
+);
+CREATE INDEX "idx_comment_content_user_id"
+ON "p_comment_content" (
+  "user_id" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_comment_reply
+-- ----------------------------
+CREATE INDEX "idx_comment_reply_comment_id"
+ON "p_comment_reply" (
+  "comment_id" ASC
 );
 
 -- ----------------------------
 -- Indexes structure for table p_contact
 -- ----------------------------
-CREATE UNIQUE INDEX "main"."idx_user_friend_id"
+CREATE UNIQUE INDEX "idx_contact_user_friend"
 ON "p_contact" (
   "user_id" ASC,
   "friend_id" ASC
 );
-CREATE INDEX "main"."idx_user_friend_status"
+CREATE INDEX "idx_contact_user_friend_status"
 ON "p_contact" (
   "user_id" ASC,
   "friend_id" ASC,
@@ -358,11 +391,131 @@ ON "p_contact" (
 );
 
 -- ----------------------------
+-- Indexes structure for table p_message
+-- ----------------------------
+CREATE INDEX "idx_message_is_read"
+ON "p_message" (
+  "is_read" ASC
+);
+CREATE INDEX "idx_message_receiver_user_id"
+ON "p_message" (
+  "receiver_user_id" ASC
+);
+CREATE INDEX "idx_message_type"
+ON "p_message" (
+  "type" ASC
+);
+
+-- ----------------------------
 -- Indexes structure for table p_post
 -- ----------------------------
-CREATE INDEX "main"."idx_visibility"
+CREATE INDEX "idx_post_user_id"
+ON "p_post" (
+  "user_id" ASC
+);
+CREATE INDEX "idx_post_visibility"
 ON "p_post" (
   "visibility" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_post_attachment_bill
+-- ----------------------------
+CREATE INDEX "idx_post_attachment_bill_post_id"
+ON "p_post_attachment_bill" (
+  "post_id" ASC
+);
+CREATE INDEX "idx_post_attachment_bill_user_id"
+ON "p_post_attachment_bill" (
+  "user_id" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_post_collection
+-- ----------------------------
+CREATE INDEX "idx_post_collection_post_id"
+ON "p_post_collection" (
+  "post_id" ASC
+);
+CREATE INDEX "idx_post_collection_user_id"
+ON "p_post_collection" (
+  "user_id" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_post_content
+-- ----------------------------
+CREATE INDEX "idx_post_content_post_id"
+ON "p_post_content" (
+  "post_id" ASC
+);
+CREATE INDEX "idx_post_content_user_id"
+ON "p_post_content" (
+  "user_id" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_post_star
+-- ----------------------------
+CREATE INDEX "idx_post_star_post_id"
+ON "p_post_star" (
+  "post_id" ASC
+);
+CREATE INDEX "idx_post_star_user_id"
+ON "p_post_star" (
+  "user_id" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_tag
+-- ----------------------------
+CREATE UNIQUE INDEX "idx_tag"
+ON "p_tag" (
+  "tag" ASC
+);
+CREATE INDEX "idx_tag_quote_num"
+ON "p_tag" (
+  "quote_num" ASC
+);
+CREATE INDEX "idx_tag_user_id"
+ON "p_tag" (
+  "user_id" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_user
+-- ----------------------------
+CREATE INDEX "idx_user_phone"
+ON "p_user" (
+  "phone" ASC
+);
+CREATE UNIQUE INDEX "idx_user_username"
+ON "p_user" (
+  "username" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_wallet_recharge
+-- ----------------------------
+CREATE INDEX "idx_wallet_recharge_trade_no"
+ON "p_wallet_recharge" (
+  "trade_no" ASC
+);
+CREATE INDEX "idx_wallet_recharge_trade_status"
+ON "p_wallet_recharge" (
+  "trade_status" ASC
+);
+CREATE INDEX "idx_wallet_recharge_user_id"
+ON "p_wallet_recharge" (
+  "user_id" ASC
+);
+
+-- ----------------------------
+-- Indexes structure for table p_wallet_statement
+-- ----------------------------
+CREATE INDEX "idx_wallet_statement_user_id"
+ON "p_wallet_statement" (
+  "user_id" ASC
 );
 
 PRAGMA foreign_keys = true;
