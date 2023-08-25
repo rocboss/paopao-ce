52a130e28170d50bc5246a3e87b7b7d8daaf9f59
Michael Li <alimy@gility.net>
2023-08-25 17:30:47 +0800

    frontend: updage package.json

--- web/dist/assets/lodash-e0b37ac3.js
+++ web/dist/assets/lodash-e0b37ac3.js
@@ -1,4 +1,4 @@
-import{c as jt}from"./copy-to-clipboard-1dd3075d.js";var Je={exports:{}};/**
+import{c as jt}from"./@babel-725317a4.js";var Je={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
