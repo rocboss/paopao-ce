e2e8c7b19462779d07632bb64c2de5c379e5789f
Michael Li <alimy@gility.net>
2023-08-30 12:12:34 +0800

    web: add custom  feature support

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -5,8 +5,10 @@
 
 interface ImportMetaEnv {
   readonly VITE_HOST: string;
+  readonly VITE_USE_FRIENDSHIP: string;
   readonly VITE_ENABLE_ANOUNCEMENT: string;
   readonly VITE_ENABLE_WALLET: string;
+  readonly VITE_ENABLE_FRIENDS_BAR: string;
   readonly VITE_ALLOW_USER_REGISTER: string;
   readonly VITE_ALLOW_PHONE_BIND: string;
   readonly VITE_ALLOW_ACTIVATION: string;
