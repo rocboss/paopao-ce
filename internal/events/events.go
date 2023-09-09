ed909ba4976d8e44311bff933149bac339d572b3
Michael Li <alimy@gility.net>
2023-09-09 10:08:18 +0800

    add events manager sub-system

---
+++ internal/events/events.go
@@ -0,0 +1,52 @@
+// Copyright 2023 ROC. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package events
+
+import (
+	"github.com/alimy/tryst/event"
+	"github.com/alimy/tryst/pool"
+	"github.com/rocboss/paopao-ce/internal/conf"
+	"github.com/sirupsen/logrus"
+)
+
+var _defaultEventManager event.EventManager
+
+// OnEvent push event to gorotine pool then handled automatic.
+func OnEvent(event event.Event) {
+	_defaultEventManager.OnEvent(event)
+}
+
+func Initial() {
+	var opts []pool.Option
+	s := conf.EventManagerSetting
+	if s.MinWorker > 5 {
+		opts = append(opts, pool.MinWorkerOpt(s.MinWorker))
+	} else {
+		opts = append(opts, pool.MinWorkerOpt(5))
+	}
+	if s.MaxEventBuf > 10 {
+		opts = append(opts, pool.MaxRequestBufOpt(s.MaxEventBuf))
+	} else {
+		opts = append(opts, pool.MaxRequestBufOpt(10))
+	}
+	if s.MaxTempEventBuf > 10 {
+		opts = append(opts, pool.MaxRequestTempBufOpt(s.MaxTempEventBuf))
+	} else {
+		opts = append(opts, pool.MaxRequestTempBufOpt(10))
+	}
+	opts = append(opts, pool.MaxTickCountOpt(s.MaxTickCount), pool.TickWaitTimeOpt(s.TickWaitTime))
+	_defaultEventManager = event.NewEventManager(func(req event.Event, err error) {
+		logrus.Errorf("handle event[%s] occurs error: %s", req.Name(), err)
+	}, opts...)
+}
+
+func Restart() {
+	_defaultEventManager.Stop()
+	_defaultEventManager.Start()
+}
+
+func Done() {
+	_defaultEventManager.Stop()
+}
