97c1d35bca3da4855c4c2adf9ae9c6456157d2fe
Michael Li <alimy@gility.net>
2023-10-01 18:28:33 +0800

    sqlx: optimize sql generate logic

--- internal/dao/sakila/yesql/gen.go
+++ internal/dao/sakila/yesql/gen.go
@@ -18,7 +18,23 @@ import (
 func main() {
 	log.Println("[Yesql] generate code start")
 	yesql.SetDefaultQueryHook(func(query *yesql.Query) (*yesql.Query, error) {
-		query.Query = strings.TrimRight(query.Query, ";")
+		buf := strings.Builder{}
+		buf.Grow(len(query.Query))
+		// 去除多余的空格
+		isPrevSpace := false
+		for _, c := range query.Query {
+			switch c {
+			case ' ', '\t':
+				if !isPrevSpace {
+					buf.WriteRune(' ')
+					isPrevSpace = true
+				}
+			default:
+				buf.WriteRune(c)
+				isPrevSpace = false
+			}
+		}
+		query.Query = strings.TrimRight(buf.String(), ";")
 		return query, nil
 	})
 	if err := yesql.Generate(); err != nil {
