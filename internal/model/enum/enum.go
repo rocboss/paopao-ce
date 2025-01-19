71d3009cab9ec51ccb6ef77cfa2ce6ecb3c08991
alimy <alimy@gility.net>
2025-01-19 14:30:18 +0800

    just add comment

--- internal/model/enum/enum.go
+++ internal/model/enum/enum.go
@@ -1,3 +1,7 @@
+// Copyright 2025 ROC. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 // Package enum all const enum variables define.
 
 package enum
