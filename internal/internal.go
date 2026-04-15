42e00623b1272dd06d1ab028dd2ffcf5b62d3591
alimy <alimy@niubiu.com>
2026-04-15 23:25:29 +0800

    just add comment for internal initial function

--- internal/internal.go
+++ internal/internal.go
@@ -10,6 +10,10 @@ import (
 	"github.com/rocboss/paopao-ce/internal/infra"
 )
 
+// Initial 内部功能模块初始化
+// Note: 目前函数很简单，只有infra需要初始化，暂时保留此函数，后续可能会
+// 有其他大的服务模块放到internal包中，需要统一在此函数中做初始化动作，而
+// 且对外保留一个internal的统一初始化接口符合直觉。
 func Initial(ctx context.Context) {
 	// infrastruct component initialize
 	infra.Initial(ctx)
